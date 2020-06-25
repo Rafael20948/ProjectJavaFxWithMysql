5243f15ea3daa1d92fdc14f0dd6cb03e4fe5f451
Rafael Miranda <rafael.silva@leega.com.br>
2020-06-25 09:12:53 -0300

    Department Service

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,19 +9,19 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+	
 	private static Scene mainScene;
 	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			ScrollPane scroolPane = loader.load();
+			ScrollPane scrollPane = loader.load();
 			
-			scroolPane.setFitToHeight(true);
-			scroolPane.setFitToWidth(true);
-		
-			mainScene = new Scene(scroolPane);
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
+			
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
@@ -29,11 +29,11 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static Scene getMainScene() {
 		return mainScene;
 	}
-
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
